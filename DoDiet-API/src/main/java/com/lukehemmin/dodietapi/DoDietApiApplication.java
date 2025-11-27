48c6792763e917974c27207a0dc44ab74bba07d1
박상호 Sangho Park <ps040211@gmail.com>
2025-11-27 14:39:25 +0900

    chore: DoDiet API 스프링 부트 초기 세팅
    - Spring Initializr 기반 Gradle 프로젝트 뼈대 생성 및 wrapper/gradle 설정 추가
    - 기본 애플리케이션 엔트리포인트, 테스트 클래스, 기본 설정 파일(application.properties) 생성
    - gitignore와 gitattributes 설정으로 빌드 산출물 및 OS별 줄바꿈/인코딩 관리

---
+++ DoDiet-API/src/main/java/com/lukehemmin/dodietapi/DoDietApiApplication.java
@@ -0,0 +1,13 @@
+package com.lukehemmin.dodietapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoDietApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoDietApiApplication.class, args);
+    }
+
+}
---
+++ DoDiet-API/src/test/java/com/lukehemmin/dodietapi/DoDietApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukehemmin.dodietapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoDietApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
